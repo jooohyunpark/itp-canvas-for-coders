21622dfa8c73db8225a431ff07e754f1280da260
jooohyunpark <jhp527@nyu.edu>
2023-06-27 22:47:16 -0400

    week 1 update

--- week1/src/index.js
+++ week1/src/index.js
@@ -13,52 +13,38 @@ app.appendChild(renderer.domElement);
 
 // scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000);
-scene.fog = new THREE.FogExp2(0x000000, 0.002);
 
 // camera
 const camera = new THREE.PerspectiveCamera(
   60,
   window.innerWidth / window.innerHeight,
   1,
-  3000
+  1000
 );
-camera.position.set(0, 0, 200);
+camera.position.set(100, 50, 200);
 
 // controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
-controls.enablePan = true;
-controls.screenSpacePanning = false;
+controls.autoRotate = true;
 controls.enableRotate = true;
 controls.enableZoom = true;
-controls.minDistance = 50;
-controls.maxDistance = 1000;
+controls.minDistance = 100;
+controls.maxDistance = 300;
 controls.maxPolarAngle = Math.PI / 2;
 
-// lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-const directionalLight = new THREE.DirectionalLight(0xff0000, 1);
-const directionalLight2 = new THREE.DirectionalLight(0x00ff00, 1);
-directionalLight.position.set(1, 1, 0);
-directionalLight.position.set(-1, -1, -1);
-scene.add(ambientLight, directionalLight, directionalLight2);
-
-// mesh
+// object
 const geometry = new THREE.TorusKnotGeometry(10, 3, 256, 32);
-const standardMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-const normalMaterial = new THREE.MeshNormalMaterial();
+const material = new THREE.MeshNormalMaterial();
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-const count = 19;
-for (let i = -Math.floor(count / 2); i < Math.ceil(count / 2); i++) {
-  const mesh = new THREE.Mesh(
-    geometry,
-    i === 0 ? normalMaterial : standardMaterial
-  );
-  mesh.position.z = i * 100;
-  scene.add(mesh);
-}
+const floorGeometry = new THREE.PlaneGeometry(30, 30);
+const floorMesh = new THREE.Mesh(floorGeometry, material);
+floorMesh.rotation.x = -Math.PI * 0.5;
+floorMesh.position.y = -30;
+scene.add(floorMesh);
 
 // resize
 const onResize = () => {
