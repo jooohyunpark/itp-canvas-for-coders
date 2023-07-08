d4bd7d6644e9612cf570f516f57c05d9e0b7a97a
jooohyunpark <jhp527@nyu.edu>
2023-07-08 00:19:16 -0400

    wip

--- week2/src/index.js
+++ week2/src/index.js
@@ -26,7 +26,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(100, 200, 400);
 
 // axis helper -> X: red, Y: green, Z: blue
-const axesHelper = new THREE.AxesHelper(100);
+const axesHelper = new THREE.AxesHelper(50);
 scene.add(axesHelper);
 
 // control
@@ -37,26 +37,26 @@ controls.screenSpacePanning = false;
 controls.enableRotate = true;
 controls.rotateSpeed = 0.3;
 controls.enableZoom = true;
-controls.zoomSpeed = 0.3;
+controls.zoomSpeed = 0.5;
 controls.minDistance = 10;
 controls.maxDistance = 1000;
 controls.maxPolarAngle = Math.PI / 2;
 
 // directional light
 const dirLight1 = new THREE.DirectionalLight("#ffff00");
-dirLight1.position.set(-100, 100, 100);
+dirLight1.position.set(-100, 100, 0);
 const dirLight1helper = new THREE.DirectionalLightHelper(dirLight1, 10);
 scene.add(dirLight1, dirLight1helper);
 
 const dirLight2 = new THREE.DirectionalLight("#0000FF");
-dirLight2.position.set(100, 100, -100);
+dirLight2.position.set(100, 50, -100);
 const dirLight2helper = new THREE.DirectionalLightHelper(dirLight2, 10);
 scene.add(dirLight2, dirLight2helper);
 
 // point light
 const pointLight = new THREE.PointLight(0xff0000, 1.5, 300);
-pointLight.position.set(-200, 20, -200);
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 3);
+pointLight.position.set(-200, 100, -200);
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 10);
 scene.add(pointLight, pointLightHelper);
 
 // ambient light
@@ -64,14 +64,16 @@ const ambientLight = new THREE.AmbientLight(0x222222);
 scene.add(ambientLight);
 
 // ground
-const floorGeometry = new THREE.PlaneGeometry(10000, 10000);
-const floorMaterial = new THREE.MeshPhongMaterial({
+const groundGeometry = new THREE.PlaneGeometry(10000, 10000);
+const groundMaterial = new THREE.MeshStandardMaterial({
   color: 0xffffff,
+  roughness: 0.8,
+  metalness: 0.2,
   side: THREE.DoubleSide,
 });
-const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
-floorMesh.rotation.x = -Math.PI * 0.5;
-scene.add(floorMesh);
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.rotation.x = -Math.PI * 0.5;
+scene.add(groundMesh);
 
 // spheres
 const geometry = new THREE.SphereGeometry(5, 128, 128);
