55a9ccf54346760ebe4eaa838b920bef5e239f1e
jooohyunpark <jhp527@nyu.edu>
2023-07-09 13:31:35 -0400

    add hemisphere light

--- week2/src/index.js
+++ week2/src/index.js
@@ -41,7 +41,7 @@ controls.enableZoom = true;
 controls.zoomSpeed = 0.5;
 controls.minDistance = 10;
 controls.maxDistance = 1000;
-controls.maxPolarAngle = Math.PI / 2;
+// controls.maxPolarAngle = Math.PI / 2;
 
 // ambient light
 const ambientLight = new THREE.AmbientLight(0xcccccc, 0.2);
@@ -54,16 +54,23 @@ scene.add(dirLight);
 const dirLighthelper = new THREE.DirectionalLightHelper(dirLight, 10);
 scene.add(dirLighthelper);
 
+// hemisphere light
+const hemiLight = new THREE.HemisphereLight(0x000000, 0x00ffff, 1);
+hemiLight.position.set(0, 200, 0);
+scene.add(hemiLight);
+const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+scene.add(hemiLightHelper);
+
 // point light
-const pointLight = new THREE.PointLight(0xff0000, 1.5, 300);
+const pointLight = new THREE.PointLight(0xff0000, 2, 300);
 pointLight.position.set(-200, 100, 0);
 scene.add(pointLight);
 const pointLightHelper = new THREE.PointLightHelper(pointLight, 10);
 scene.add(pointLightHelper);
 
 // area light
-const rectLight = new THREE.RectAreaLight(0x00ff00, 3, 30, 100);
-rectLight.position.set(0, 0, 200);
+const rectLight = new THREE.RectAreaLight(0x00ff00, 3, 50, 100);
+rectLight.position.set(0, 50, 200);
 scene.add(rectLight);
 const rectLightHelper = new RectAreaLightHelper(rectLight);
 scene.add(rectLightHelper);
@@ -71,7 +78,7 @@ scene.add(rectLightHelper);
 // spot light
 const spotLight = new THREE.SpotLight(0xffff00, 3);
 spotLight.angle = Math.PI * 0.1;
-spotLight.penumbra = 0.2;
+spotLight.penumbra = 0.3;
 spotLight.decay = 2;
 spotLight.distance = 300;
 spotLight.position.set(100, 100, -300);
@@ -96,7 +103,7 @@ scene.add(groundMesh);
 // spheres
 const geometry = new THREE.SphereGeometry(5, 128, 128);
 const material = new THREE.MeshPhongMaterial({
-  color: 0x000000,
+  color: 0xffffff,
 });
 for (let i = 0; i < 30; i++) {
   const mesh = new THREE.Mesh(geometry, material);
