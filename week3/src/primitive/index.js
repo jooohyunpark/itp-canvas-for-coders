7c07aa44da532e6676f1294128d21c1048f10c2e
Joohyun Park <jhp527@nyu.edu>
2023-08-24 19:41:19 -0400

    text mesh wip

--- week3/src/primitive/index.js
+++ week3/src/primitive/index.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import helvetiker_regular from "three/examples/fonts/helvetiker_regular.typeface.json";
 
 // app
 const app = document.querySelector("#app");
@@ -20,9 +23,9 @@ const camera = new THREE.PerspectiveCamera(
   60,
   window.innerWidth / window.innerHeight,
   1,
-  3000
+  1000
 );
-camera.position.set(20, 20, 40);
+camera.position.set(20, 10, 15);
 scene.add(camera);
 
 // axis helper -> X: red, Y: green, Z: blue
@@ -63,7 +66,7 @@ scene.add(planeMesh);
 // box
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMesh = new THREE.Mesh(boxGeometry, material);
-planeMesh.position.z = -5;
+boxMesh.position.z = -5;
 scene.add(boxMesh);
 
 // sphere
@@ -71,6 +74,40 @@ const sphereGeometry = new THREE.SphereGeometry(1, 32, 16);
 const sphereMesh = new THREE.Mesh(sphereGeometry, material);
 sphereMesh.position.z = -10;
 scene.add(sphereMesh);
+
+// cynlinder
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+const cylinderMesh = new THREE.Mesh(cylinderGeometry, material);
+cylinderMesh.position.z = -15;
+scene.add(cylinderMesh);
+
+// torusknot
+const torusKnotGeometry = new THREE.TorusKnotGeometry(1, 0.3, 64, 16);
+const torusKnotMesh = new THREE.Mesh(torusKnotGeometry, material);
+torusKnotMesh.position.z = -20;
+scene.add(torusKnotMesh);
+
+// text
+const fontLoader = new FontLoader();
+fontLoader.load(helvetiker_regular, (font) => {
+  console.log(font);
+
+  const textGeometry = new TextGeometry("Hello three.js!", {
+    font: font,
+    size: 3,
+    height: 5,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 10,
+    bevelSize: 8,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+
+  const textMesh = new THREE.Mesh(textGeometry, material);
+  textMesh.position.z = -25;
+  scene.add(textMesh);
+});
 
 // animate
 const animate = () => {
