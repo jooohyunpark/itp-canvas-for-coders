d2fe3d67c650b5da48fb2f0da00a06b3718bf9e6
Joohyun Park <jhp527@nyu.edu>
2023-08-26 17:18:16 -0400

    add exercise

---
+++ week3/src/exercise/index.js
@@ -0,0 +1,68 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls";
+
+// app
+const app = document.querySelector("#app");
+
+// renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+app.appendChild(renderer.domElement);
+
+// scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+
+// perspective camera
+const camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+camera.position.set(10, 10, 20);
+scene.add(camera);
+
+// axis helper -> X: red, Y: green, Z: blue
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
+
+// ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+// control
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.enableRotate = true;
+controls.rotateSpeed = 0.5;
+controls.enableZoom = true;
+
+// resize
+const onResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+window.addEventListener("resize", onResize);
+
+// box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshNormalMaterial();
+const boxMesh = new THREE.Mesh(boxGeometry, material);
+scene.add(boxMesh);
+
+// animate
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+  controls.update();
+};
+
+animate();
