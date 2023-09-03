6207c66de11daf5daa7201587fe644e21a07e371
Joohyun Park <jhp527@nyu.edu>
2023-09-03 13:06:26 -0400

    add sub folders

--- week4/vite.config.js
+++ week4/vite.config.js
@@ -8,5 +8,11 @@ export default defineConfig({
   },
   build: {
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        gsap: resolve(__dirname, "src/gsap/index.html"),
+      },
+    },
   },
 });
