b7faf699b31bbe0fe83d742dda1ddc09e45b2e28
Joohyun Park <jhp527@nyu.edu>
2023-09-11 21:19:51 -0400

    add shadow

--- week5/src/shadows/index.js
+++ week5/src/shadows/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // app
 const app = document.querySelector("#app");
@@ -27,9 +28,10 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(20, 10, 20);
 scene.add(camera);
 
-// axis helper -> X: red, Y: green, Z: blue
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+// // axis helper -> X: red, Y: green, Z: blue
+// const axesHelper = new THREE.AxesHelper(5);
+// axesHelper.position.y = 0.001;
+// scene.add(axesHelper);
 
 // ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -67,6 +69,7 @@ controls.rotateSpeed = 0.5;
 controls.enableZoom = true;
 controls.minDistance = 10;
 controls.maxDistance = 100;
+controls.target = new THREE.Vector3(0, 4, 0);
 
 // resize
 const onResize = () => {
@@ -81,20 +84,13 @@ window.addEventListener("resize", onResize);
 //////////////////////////////////////////////////////////////////////////////
 */
 
-// geometry
-const geometry = new THREE.TorusKnotGeometry(1, 0.3, 128, 64);
-
-// MeshNormalMaterial
-const mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial());
-mesh.position.y = 5;
-mesh.castShadow = true;
-scene.add(mesh);
+const bananaGroup = new THREE.Group();
 
 // floor
 const floorGeometry = new THREE.PlaneGeometry(2000, 2000);
 const floorMaterial = new THREE.MeshStandardMaterial({
-  color: "white",
-  roughness: 0.2,
+  color: "#ccc",
+  roughness: 0.8,
   metalness: 0,
   side: THREE.DoubleSide,
 });
@@ -103,13 +99,74 @@ floorMesh.rotation.x = Math.PI * 0.5;
 floorMesh.receiveShadow = true;
 scene.add(floorMesh);
 
+// Instantiate a loader
+const loader = new GLTFLoader();
+
+// Load a glTF resource
+loader.load(
+  // resource URL
+  "/banana.glb",
+  // called when the resource is loaded
+  function (gltf) {
+    gltf.scene.position.y = 4.2;
+    gltf.scene.scale.setScalar(0.75);
+
+    gltf.scene.traverse(function (el) {
+      if (el.isMesh) {
+        el.castShadow = true;
+        el.receiveShadow = true;
+      }
+    });
+
+    // target directional light to banana object
+    // directionalLight.target = gltf.scene;
+    // scene.add(directionalLight.target);
+
+    bananaGroup.add(gltf.scene);
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+// box
+const boxGeometry = new THREE.BoxGeometry(2, 4, 2);
+const boxMaterial = new THREE.MeshStandardMaterial({ color: "white" });
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.position.y = 2;
+boxMesh.castShadow = true;
+boxMesh.receiveShadow = true;
+
+bananaGroup.add(boxMesh);
+scene.add(bananaGroup);
+
+// torusknot
+const torusKnotMesh = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(1, 0.3, 128, 64),
+  new THREE.MeshNormalMaterial()
+);
+torusKnotMesh.scale.setScalar(0.5);
+torusKnotMesh.position.set(0, 4, -6);
+torusKnotMesh.castShadow = true;
+scene.add(torusKnotMesh);
+
 /* 
 //////////////////////////////////////////////////////////////////////////////
 */
 
 // animate
-const animate = () => {
+const animate = (time) => {
   requestAnimationFrame(animate);
+
+  time *= 0.001;
+
+  bananaGroup.rotation.y = Math.PI * time * 0.1;
+  torusKnotMesh.rotation.y = Math.PI * time * -0.1;
 
   renderer.render(scene, camera);
   controls.update();
