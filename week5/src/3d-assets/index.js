7971793b43391a4052d9777f1cc203bcbef13703
Joohyun Park <jhp527@nyu.edu>
2023-09-12 23:18:48 -0400

    add positional audio

--- week5/src/3d-assets/index.js
+++ week5/src/3d-assets/index.js
@@ -109,6 +109,56 @@ boxMesh.position.y = 2;
 scene.add(boxMesh);
 
 /* 
+Positional Audio
+*/
+
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+// create the PositionalAudio object (passing in the listener)
+const sound = new THREE.PositionalAudio(listener);
+const sound2 = new THREE.PositionalAudio(listener);
+
+// load a sound and set it as the PositionalAudio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load("/forest.wav", function (buffer) {
+  sound.setBuffer(buffer);
+  sound.setVolume(0.7);
+  sound.setRefDistance(10);
+  sound.setRolloffFactor(5);
+  sound.setLoop(true);
+  sound.play();
+});
+
+audioLoader.load("/rain.wav", function (buffer) {
+  sound2.setBuffer(buffer);
+  sound2.setVolume(0.7);
+  sound2.setRefDistance(5);
+  sound2.setRolloffFactor(3);
+  sound2.setLoop(true);
+  sound2.play();
+});
+
+// sphere
+const sphereGeometry = new THREE.SphereGeometry(1, 128, 128);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  color: "#5200ff",
+  roughness: 0.2,
+  metalness: 0.1,
+});
+
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphereMesh.position.set(20, 5, 0);
+sphereMesh.add(sound);
+scene.add(sphereMesh);
+
+const sphereMesh2 = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphereMesh2.position.set(-30, 5, -20);
+sphereMesh2.add(sound2);
+scene.add(sphereMesh2);
+
+/* 
 //////////////////////////////////////////////////////////////////////////////
 */
 
