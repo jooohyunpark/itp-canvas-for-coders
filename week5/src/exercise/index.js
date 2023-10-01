af2e22ff5998856ef7e82f840b379f4b6c2ea2ac
Joohyun Park <jhp527@nyu.edu>
2023-09-30 23:24:36 -0400

    add exercise in week5

--- week5/src/exercise/index.js
+++ week5/src/exercise/index.js
@@ -74,40 +74,9 @@ window.addEventListener("resize", onResize);
 // Instantiate a loader
 const loader = new GLTFLoader();
 
-// animation mixer
+// Instantiate animation mixer
 let mixer;
 const clock = new THREE.Clock();
-
-// Load a glTF resource
-loader.load(
-  // resource URL
-  "/banana.glb",
-  // called when the resource is loaded
-  function (gltf) {
-    console.log(`banana gltf: `, gltf);
-
-    scene.add(gltf.scene);
-
-    gltf.scene.position.y = 4.2;
-    gltf.scene.scale.setScalar(0.75);
-
-    gltf.scene.traverse(function (el) {
-      console.log("traverse: ", el);
-
-      if (el.isMesh) {
-        console.log("isMesh: ", el);
-      }
-    });
-  },
-  // called while loading is progressing
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  // called when loading has errors
-  function (error) {
-    console.log("An error happened");
-  }
-);
 
 const fisthGroup = new THREE.Group();
 
@@ -116,86 +85,20 @@ loader.load("/fish.glb", function (gltf) {
 
   fisthGroup.add(gltf.scene);
 
-  gltf.scene.position.set(0, 10, 10);
-  gltf.scene.scale.setScalar(5);
-
   // initiate animation mixer
   mixer = new THREE.AnimationMixer(gltf.scene.children[0]);
   mixer.clipAction(gltf.animations[0]).play();
+
+  gltf.scene.position.set(0, 10, 10);
+  gltf.scene.scale.setScalar(5);
 });
 
-// rotate fishgroup
 gsap.to(fisthGroup.rotation, {
   y: Math.PI * 2,
   duration: 60,
   repeat: -1,
 });
 scene.add(fisthGroup);
-
-// box
-const boxGeometry = new THREE.BoxGeometry(2, 4, 2);
-const boxMaterial = new THREE.MeshStandardMaterial({ color: "white" });
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.position.y = 2;
-scene.add(boxMesh);
-
-/* 
-Positional Audio
-*/
-
-// create an AudioListener and add it to the camera
-const listener = new THREE.AudioListener();
-camera.add(listener);
-
-// create the PositionalAudio object (passing in the listener)
-const sound = new THREE.PositionalAudio(listener);
-const sound2 = new THREE.PositionalAudio(listener);
-
-// load a sound and set it as the PositionalAudio object's buffer
-const audioLoader = new THREE.AudioLoader();
-
-audioLoader.load("/forest.mp3", function (buffer) {
-  sound.setBuffer(buffer);
-  sound.setVolume(0.7);
-  sound.setRefDistance(10);
-  sound.setRolloffFactor(5);
-  sound.setLoop(true);
-});
-audioLoader.load("/rain.mp3", function (buffer) {
-  sound2.setBuffer(buffer);
-  sound2.setVolume(1);
-  sound2.setRefDistance(5);
-  sound2.setRolloffFactor(1);
-  sound2.setLoop(true);
-});
-
-// start playing on user interaction - https://developer.chrome.com/blog/autoplay/#webaudio
-const play = () => {
-  if (sound.buffer && !sound.isPlaying) sound.play();
-  if (sound2.buffer && !sound2.isPlaying) sound2.play();
-};
-window.addEventListener("click", play);
-
-// sphere
-const sphereGeometry = new THREE.SphereGeometry(1, 128, 128);
-const sphereMaterial = new THREE.MeshStandardMaterial({
-  color: "cyan",
-  roughness: 0.2,
-});
-const sphereMaterial2 = new THREE.MeshStandardMaterial({
-  color: "magenta",
-  roughness: 0.2,
-});
-
-const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphereMesh.position.set(20, 5, 0);
-sphereMesh.add(sound);
-scene.add(sphereMesh);
-
-const sphereMesh2 = new THREE.Mesh(sphereGeometry, sphereMaterial2);
-sphereMesh2.position.set(-30, 5, -20);
-sphereMesh2.add(sound2);
-scene.add(sphereMesh2);
 
 /* 
 //////////////////////////////////////////////////////////////////////////////
--- week5/vite.config.js
+++ week5/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         "3d-assets": resolve(__dirname, "src/3d-assets/index.html"),
         shadows: resolve(__dirname, "src/shadows/index.html"),
+        exercise: resolve(__dirname, "src/exercise/index.html"),
       },
     },
   },
