060b639c081a9fcc2a7f87669b164d42cfeb49c8
Joohyun Park <jhp527@nyu.edu>
2023-10-08 01:17:24 -0400

    adjust axis-y

--- week3/src/exercise/index.js
+++ week3/src/exercise/index.js
@@ -27,7 +27,7 @@ scene.add(camera);
 
 // axis helper -> X: red, Y: green, Z: blue
 const axesHelper = new THREE.AxesHelper(5);
-axesHelper.position.y = 0.001; // so we're above the ground slightly
+axesHelper.position.y = 0.001; // above the ground slightly
 scene.add(axesHelper);
 
 // grid helper
