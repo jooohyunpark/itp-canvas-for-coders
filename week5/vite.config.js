359b178b507acbbf08feb403e1f2d05db2c14be1
Joohyun Park <jhp527@nyu.edu>
2024-06-26 21:17:26 -0400

    support open in dev server

--- week5/vite.config.js
+++ week5/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 3000,
+    open: true,
   },
   root: resolve(__dirname, "src"),
   publicDir: resolve(__dirname, "public"),
