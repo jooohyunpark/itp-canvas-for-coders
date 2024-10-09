ff0c143c50c83ea3eff3cd2c07cd7a73ee965313
Joohyun Park <jhp527@nyu.edu>
2024-10-08 20:08:33 -0400

    evenly distribute sphere coordinates

--- week6/src/index.js
+++ week6/src/index.js
@@ -59,6 +59,23 @@ const onResize = () => {
 };
 window.addEventListener("resize", onResize);
 
+// https://karthikkaranth.me/blog/generating-random-points-in-a-sphere/
+const getPoint = (radius = 1) => {
+  const u = Math.random();
+  const v = Math.random();
+  const theta = u * 2.0 * Math.PI;
+  const phi = Math.acos(2.0 * v - 1.0);
+  const r = Math.cbrt(Math.random()) * radius;
+  const sinTheta = Math.sin(theta);
+  const cosTheta = Math.cos(theta);
+  const sinPhi = Math.sin(phi);
+  const cosPhi = Math.cos(phi);
+  const x = r * sinPhi * cosTheta;
+  const y = r * sinPhi * sinTheta;
+  const z = r * cosPhi;
+  return { x, y, z };
+};
+
 // spheres
 const sphereGeometry = new THREE.SphereGeometry(2, 128, 128);
 const sphereMaterial = new THREE.MeshStandardMaterial({
@@ -69,9 +86,7 @@ const sphereMaterial = new THREE.MeshStandardMaterial({
 
 for (let i = 0; i < 1000; i++) {
   const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial.clone());
-  const x = 100 - Math.random() * 200;
-  const y = 100 - Math.random() * 200;
-  const z = 100 - Math.random() * 200;
+  const { x, y, z } = getPoint(100);
   sphereMesh.position.set(x, y, z);
 
   // adding custome properties to use later
