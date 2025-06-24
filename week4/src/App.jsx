b2eba6047b216af27b97d1532cf61893a01e365b
Joohyun Park <jhp527@nyu.edu>
2025-06-24 11:17:28 -0400

    basic react example

--- week4/src/App.jsx
+++ week4/src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
 function App() {
-  return <>App</>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div id="app">
+      <Button onClick={() => setCount(count + 1)}>Click me</Button>
+
+      {count}
+    </div>
+  );
 }
 
 export default App;
---
+++ week4/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import styles from "./Button.module.css";
+
+const Button = ({ children, ...props }) => {
+  return (
+    <button className={styles.button} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
