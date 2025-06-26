bff02f71555b7ec939c8a1d286ca81ce4bd60412
Joohyun Park <jhp527@nyu.edu>
2025-06-26 11:19:50 -0400

    week 5 wip

--- week5/src/pages/Drei.jsx
+++ week5/src/pages/Drei.jsx
@@ -1,23 +1,51 @@
 import { useRef } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera, useHelper } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { PointLightHelper, DirectionalLightHelper } from "three";
-import Box from "@/components/Box";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  Center,
+  RandomizedLight,
+  AccumulativeShadows,
+  Stage,
+} from "@react-three/drei";
+import { useSpring, animated } from "@react-spring/three";
 
 function Scene() {
   return (
     <>
-      <color attach="background" args={["white"]} />
-
       <PerspectiveCamera makeDefault fov={60} position={[0, 0, 10]} />
-      <OrbitControls makeDefault />
+      <OrbitControls
+        makeDefault
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 2}
+        target={[0, 0.5, 0]}
+      />
 
-      <ambientLight intensity={1} />
-
-      <Box position={[-2, 0, 0]} />
-      <Box position={[0, 0, 0]} />
-      <Box position={[2, 0, 0]} />
+      <Stage
+        intensity={0.5}
+        preset="rembrandt"
+        shadows={{
+          type: "accumulative",
+          color: "skyblue",
+          colorBlend: 2,
+          opacity: 1,
+        }}
+        adjustCamera={false}
+        environment="city"
+      >
+        <Center top position={[-2, 0, 0]}>
+          <mesh castShadow>
+            <sphereGeometry args={[0.25, 64, 64]} />
+            <meshStandardMaterial color="lightblue" />
+          </mesh>
+        </Center>
+        <Center top position={[2.5, 0, 0]}>
+          <animated.mesh castShadow rotation={[0, Math.PI / 4, 0]}>
+            <boxGeometry args={[0.5, 0.5, 0.5]} />
+            <meshStandardMaterial color="lightblue" />
+          </animated.mesh>
+        </Center>
+      </Stage>
     </>
   );
 }
@@ -25,7 +53,7 @@ function Scene() {
 function App() {
   return (
     <div id="app">
-      <Canvas>
+      <Canvas shadows>
         <Scene />
       </Canvas>
     </div>
