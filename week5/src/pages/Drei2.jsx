0eda8a5018a2ac16256b15f954622373cb7ca3b9
Joohyun Park <jhp527@nyu.edu>
2025-08-09 10:01:22 -0400

    prep scrollcontrol

--- week5/src/pages/Drei2.jsx
+++ week5/src/pages/Drei2.jsx
@@ -1,20 +1,36 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  ScrollControls,
+  useScroll,
+  Stage,
+} from "@react-three/drei";
+import Human from "@/components/Human";
 
 function Scene() {
+  const scroll = useScroll();
+
+  useFrame((state, delta) => {
+    const scrollProgress = scroll.offset;
+
+    console.log(scrollProgress);
+
+    state.camera.position.z = 7 + scrollProgress * -5; // Full rotation
+  });
   return (
     <>
       <PerspectiveCamera makeDefault fov={60} position={[0, 0, 7]} />
-      <OrbitControls
+      {/* <OrbitControls
         makeDefault
         minPolarAngle={0}
         maxPolarAngle={Math.PI / 2}
         target={[0, 0.5, 0]}
-      />
+      /> */}
 
-      {/* scroll camera 
-      HTML component
-       */}
+      <Stage intensity={0.7} adjustCamera={false} environment="city">
+        <Human position={[0, 0, 0]} />
+      </Stage>
     </>
   );
 }
@@ -23,7 +39,9 @@ function App() {
   return (
     <div id="app">
       <Canvas shadows>
-        <Scene />
+        <ScrollControls pages={3} damping={0.25}>
+          <Scene />
+        </ScrollControls>
       </Canvas>
     </div>
   );
