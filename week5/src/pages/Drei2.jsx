7442910442d65094095282fbf036e3359e49cee5
Joohyun Park <jhp527@nyu.edu>
2025-08-13 22:59:46 -0400

    fine tune decay trail

--- week5/src/pages/Drei2.jsx
+++ week5/src/pages/Drei2.jsx
@@ -67,7 +67,7 @@ function Scene() {
           width={0.2} // Width of the line
           color={"blue"} // Color of the line
           length={2} // Length of the line
-          decay={0.4} // How fast the line fades away
+          decay={0.5} // How fast the line fades away
           local={false} // Wether to use the target's world or local positions
           stride={0} // Min distance between previous and current point
           interval={1} // Number of frames to wait before next calculation
