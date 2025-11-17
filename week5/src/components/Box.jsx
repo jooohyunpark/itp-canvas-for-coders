ac6d001375d673415dac26738d5c7108e5a52468
Joohyun Park <jhp527@nyu.edu>
2025-11-16 20:43:27 -0500

    clean code

--- week5/src/components/Box.jsx
+++ week5/src/components/Box.jsx
@@ -4,11 +4,15 @@ import { useFrame } from "@react-three/fiber";
 const Box = ({ color = "white", ...props }) => {
   // This reference will give us direct access to the mesh
   const meshRef = useRef();
+
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
+
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta * 0.5));
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta * 0.5;
+  });
 
   return (
     <mesh
